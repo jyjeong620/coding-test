7069463070410cd8601dfa879dad3df475ffb86e
정준영 <jeongjy@mrblue.com>
2025-04-01 08:46:58 +0900

    feat: 약수의 합

--- src/main/java/junyoung/week1/Solution.java
+++ src/main/java/junyoung/week1/Solution.java
@@ -1,8 +1,11 @@
 package junyoung.week1;
 
+import java.util.stream.IntStream;
+
 public class Solution {
-    public int solution(int n) {
-        // Test용
-        return 0;
+    public int sumDivisor(int n) {
+        return IntStream.rangeClosed(1, n)
+                .filter(i -> n % i == 0)
+                .sum();
     }
 }
--- src/test/java/junyoung/week1/SolutionTest.java
+++ src/test/java/junyoung/week1/SolutionTest.java
@@ -1,12 +1,27 @@
 package junyoung.week1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
-    @Test
-    void testSolution() {
+    @DisplayName("약수의 합 테스트")
+    @ParameterizedTest
+    @CsvSource({
+            "12, 28",
+            "5, 6",
+    })
+    void sumDivisor1(int n, int expected) {
+        // given
+        Solution solution = new Solution();
+
+        // when
+        int result = solution.sumDivisor(n);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 
 }
