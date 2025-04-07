b90c9659bac576b197eeef678329b032d470776b
devyouj <devyouj@gmail.com>
2025-04-07 22:00:02 +0900

    feat: 평균 구하기

--- src/main/java/yujin/week2/Solution.java
+++ src/main/java/yujin/week2/Solution.java
@@ -1,12 +1,14 @@
 package yujin.week2;
 
+import java.util.Arrays;
+
 public class Solution {
     public String oddOrEven(int num) {
         return num % 2 == 0 ? "Even" : "Odd";
     }
 
-    public double average(int[] numbers) {
-        return 0;
+    public double average(int[] arr) {
+        return Arrays.stream(arr).average().getAsDouble();
     }
 
     public long[] numberSequence(long number, int size) {
