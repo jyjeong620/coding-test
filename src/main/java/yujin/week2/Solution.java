2e3a81a033e56678b9692ae52cd85cabba52a034
devyouj <devyouj@gmail.com>
2025-04-09 21:54:20 +0900

    feat: 추억 점수

--- src/main/java/yujin/week2/Solution.java
+++ src/main/java/yujin/week2/Solution.java
@@ -13,9 +13,27 @@ public class Solution {
 
     public long[] numberSequence(int x, int n) {
         long[] numbers = new long[n];
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             numbers[i] = (long) x * (i + 1);
         }
         return numbers;
+    }
+
+    public int[] memoryPoints(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+
+        for (int i = 0; i < photo.length; i++) {
+            int point = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                for (int n = 0; n < name.length; n++) {
+                    if (name[n].equals(photo[i][j])) {
+                        point += yearning[n];
+                    }
+                }
+            }
+            answer[i] = point;
+        }
+
+        return answer;
     }
 }
--- src/test/java/yujin/week2/SolutionTest.java
+++ src/test/java/yujin/week2/SolutionTest.java
@@ -2,9 +2,12 @@ package yujin.week2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -70,4 +73,55 @@ class SolutionTest {
         // then
         assertThat(result).isEqualTo(expected);
     }
+
+    @DisplayName("추억 점수")
+    @ParameterizedTest
+    @MethodSource("memoryPointsTestCases")
+    void memoryPoints(String[] name, int[] yearning, String[][] photo, int[] expected) {
+        // given
+        Solution solution = new Solution();
+
+        // when
+        int[] result = solution.memoryPoints(name, yearning, photo);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> memoryPointsTestCases() {
+        return Stream.of(
+                Arguments.of(
+                        new String[]{"may", "kein", "kain", "radi"},
+                        new int[]{5, 10, 1, 3},
+                        new String[][]{
+                                {"may", "kein", "kain", "radi"},
+                                {"may", "kein", "brin", "deny"},
+                                {"kon", "kain", "may", "coni"}
+                        },
+                        new int[]{19, 15, 6}
+                ),
+                Arguments.of(
+                        new String[]{"kali", "mari", "don"},
+                        new int[]{11, 1, 55},
+                        new String[][]{
+                                {"kali", "mari", "don"},
+                                {"pony", "tom", "teddy"},
+                                {"con", "mona", "don"}
+                        },
+                        new int[]{67, 0, 55}
+                ),
+                Arguments.of(
+                        new String[]{"may", "kein", "kain", "radi"},
+                        new int[]{5, 10, 1, 3},
+                        new String[][]{
+                                {"may"},
+                                {"kein", "deny", "may"},
+                                {"kon", "coni"}
+                        },
+                        new int[]{5, 15, 0}
+                )
+        );
+    }
 }
+
+
