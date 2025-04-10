6644fea90bf9946b22dabc0b20bb556fa6835780
정준영 <jeongjy@mrblue.com>
2025-04-10 09:02:39 +0900

    feat: 개인정보 수집 유효기간

--- src/test/java/junyoung/week2/SolutionTest.java
+++ src/test/java/junyoung/week2/SolutionTest.java
@@ -2,7 +2,12 @@ package junyoung.week2;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,4 +30,24 @@ class SolutionTest {
         assertThat(actual).isEqualTo(expected);
     }
 
+    @DisplayName("개인정보 수집 유효기간")
+    @ParameterizedTest
+    @MethodSource("provideTestCases")
+    void getDestructionPersonalInfo(String today, String[] terms, String[] privacies, int[] expected) {
+        // given
+        Solution solution = new Solution();
+
+        // when
+        int[] actual = solution.getDestructionPersonalInfo(today, terms, privacies);
+
+        // then
+        assertThat(actual).contains(expected);
+    }
+
+    private static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+                Arguments.of("2022.05.19",new String[] {"A 6", "B 12", "C 3"}, new String[] {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"}, new int[]{1, 3}),
+                Arguments.of("2020.01.01", new String[] {"Z 3", "D 5"}, new String[] {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"}, new int[]{1, 4, 5})
+        );
+    }
 }
