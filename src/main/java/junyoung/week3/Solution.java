33b9e932ef0b6caa230d32ce0da6b999dd133ffc
정준영 <jeongjy@mrblue.com>
2025-04-16 08:57:57 +0900

    feat: 카드뭉치

--- src/main/java/junyoung/week3/Solution.java
+++ src/main/java/junyoung/week3/Solution.java
@@ -34,4 +34,19 @@ public class Solution {
         }
         return sevenStep;
     }
+
+    public String card(String[] cards1, String[] cards2, String[] goal) {
+        int i = 0;
+        int j = 0;
+        for (String word : goal) {
+            if (i < cards1.length && word.equals(cards1[i])) {
+                i++;
+            } else if (j < cards2.length && word.equals(cards2[j])) {
+                j++;
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
+    }
 }
--- src/test/java/junyoung/week3/SolutionTest.java
+++ src/test/java/junyoung/week3/SolutionTest.java
@@ -3,6 +3,7 @@ package junyoung.week3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -26,5 +27,27 @@ class SolutionTest {
 
         // then
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("카드 뭉치")
+    @ParameterizedTest
+    @MethodSource("cardProvider")
+    void card(String[] cards1, String[] cards2, String[] goal, String expected) {
+        // given
+        Solution solution = new Solution();
+
+        // when
+        String actual = solution.card(cards1, cards2, goal);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Object[][] cardProvider() {
+        return new Object[][]{
+                {new String[]{"i", "drink", "water"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"}, "Yes"},
+                {new String[]{"i", "water", "drink"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"}, "No"},
+                {new String[]{"i", "water"}, new String[]{"want", "to"}, new String[]{"i", "want", "to", "drink", "water"}, "No"},
+        };
     }
 }
