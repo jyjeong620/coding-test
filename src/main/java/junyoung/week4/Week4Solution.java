8111f65870433f7e46daa739886e713f484a9875
정준영 <jeongjy@mrblue.com>
2025-04-23 09:22:33 +0900

    feat: 모의고사

--- src/main/java/junyoung/week4/Week4Solution.java
+++ src/main/java/junyoung/week4/Week4Solution.java
@@ -10,4 +10,50 @@ public class Week4Solution {
         }
         return "김서방은 없다";
     }
+
+    public int[] mockExam(int[] answers) {
+        int[] mathHater1 = new int[]{1, 2, 3, 4, 5};
+        int[] mathHater2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5};
+        int[] mathHater3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int answerCount1 = grade(answers, mathHater1);
+        int answerCount2 = grade(answers, mathHater2);
+        int answerCount3 = grade(answers, mathHater3);
+
+        return findMaxIndices(answerCount1, answerCount2, answerCount3);
+    }
+
+    private int grade(int[] answers, int[] mathHater1) {
+        int length = mathHater1.length;
+        int count = 0;
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == mathHater1[i % length]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int[] findMaxIndices(int a, int b, int c) {
+        int[] arr = {a, b, c};
+        int max = Math.max(Math.max(a, b), c);
+
+        int count = 0;
+        for (int num : arr) {
+            if (num == max) {
+                count++;
+            }
+        }
+
+        int[] result = new int[count];
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) {
+                result[index++] = i + 1; // 1부터 시작하는 인덱스로 변환
+            }
+        }
+
+        return result;
+    }
 }
--- src/test/java/junyoung/week4/Week4SolutionTest.java
+++ src/test/java/junyoung/week4/Week4SolutionTest.java
@@ -2,6 +2,8 @@ package junyoung.week4;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,4 +24,24 @@ class Week4SolutionTest {
         assertThat(actual).isEqualTo("김서방은 1에 있다");
     }
 
+    @DisplayName("모의고사")
+    @ParameterizedTest
+    @MethodSource("mockExamProvider")
+    void mockExam(int[] answers, int[] expected) {
+        // given
+        Week4Solution solution = new Week4Solution();
+
+        // when
+        int[] actual = solution.mockExam(answers);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    private static Object[][] mockExamProvider() {
+        return new Object[][]{
+                {new int[]{1,2,3,4,5}, new int[]{1}},
+                {new int[]{1,3,2,4,2}, new int[]{1,2,3}}
+        };
+    }
 }
