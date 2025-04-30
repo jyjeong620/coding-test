cab4157796884ff42c55d97f0379ba4a8e45ffcf
정준영 <jeongjy@mrblue.com>
2025-04-30 09:20:58 +0900

    feat: 햄버거 만들

--- src/main/java/junyoung/week5/Week5Solution.java
+++ src/main/java/junyoung/week5/Week5Solution.java
@@ -1,5 +1,7 @@
 package junyoung.week5;
 
+import java.util.LinkedList;
+
 public class Week5Solution {
     public String ourPassword(String s, String skip, int index) {
         char[] inputs = s.toCharArray();
@@ -22,6 +24,28 @@ public class Week5Solution {
             alpha = alpha.replace(s, "");
         }
         return alpha;
+    }
+
+    public int makeHamburger(int[] ingredient) {
+        int makeCount = 0;
+
+        LinkedList<Integer> objects = new LinkedList<>();
+        for (int i : ingredient) {
+            objects.add(i);
+            int size = objects.size();
+            if (size > 4
+                    && objects.getLast() == 1
+                    && objects.get(size - 2) == 3
+                    && objects.get(size - 3) == 2
+                    && objects.get(size - 4) == 1
+            ) {
+                makeCount++;
+                for (int j = 0; j < 4; j++) {
+                    objects.removeLast();
+                }
+            }
+        }
+        return makeCount;
     }
 }
 
--- src/test/java/junyoung/week5/Week5SolutionTest.java
+++ src/test/java/junyoung/week5/Week5SolutionTest.java
@@ -2,7 +2,11 @@ package junyoung.week5;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +16,7 @@ class Week5SolutionTest {
     @DisplayName("둘만의 암호")
     @ParameterizedTest
     @CsvSource({
-        "aukks, wbqd, 5, happy"
+            "aukks, wbqd, 5, happy"
     })
     void ourPasswordTest(String s, String skip, int index, String expected) {
         // given
@@ -25,4 +29,24 @@ class Week5SolutionTest {
         assertThat(actual).isEqualTo(expected);
     }
 
+    @DisplayName("햄버거 만들기")
+    @ParameterizedTest
+    @MethodSource("makeHamburgerProvider")
+    void makeHamburgerTest(int[] ingredients, int expected) {
+        // given
+        Week5Solution solution = new Week5Solution();
+
+        // when
+        int actual = solution.makeHamburger(ingredients);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> makeHamburgerProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1}, 2),
+                Arguments.of(new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2}, 0)
+        );
+    }
 }
