7d934c0fadc4489d66c7443bbd66c62ae9736772
devyouj <devyouj@gmail.com>
2025-05-06 18:26:08 +0900

    refactor: 시저 암호

--- src/main/java/yujin/week6/Week6Solution.java
+++ src/main/java/yujin/week6/Week6Solution.java
@@ -1,36 +1,19 @@
 package yujin.week6;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Week6Solution {
     public String caesarPassword(String s, int n) {
-        char[] targets = s.toCharArray();
-        List<Character> lowerAlphabets = new ArrayList<>();
-        List<Character> upperAlphabets = new ArrayList<>();
-        for (int i = 0; i < 26; i++) {
-            lowerAlphabets.add((char) ('a' + i));
-            upperAlphabets.add((char) ('A' + i));
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < targets.length; i++) {
-            if (targets[i] == ' ') {
-                sb.append(' ');
-                continue;
-            }
-
-            if (lowerAlphabets.contains(targets[i])) {
-                int index = lowerAlphabets.indexOf(targets[i]);
-                int indexOfCaesar = (index + n) % 26;
-                sb.append(lowerAlphabets.get(indexOfCaesar));
+        StringBuilder answer = new StringBuilder();
+        for (char ch : s.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                char password = (char) ((ch - 'a' + n) % 26 + 'a');
+                answer.append(password);
+            } else if (Character.isUpperCase(ch)) {
+                char password = (char) ((ch - 'A' + n) % 26 + 'A');
+                answer.append(password);
             } else {
-                int index = upperAlphabets.indexOf(targets[i]);
-                int indexOfCaesar = (index + n) % 26;
-                sb.append(upperAlphabets.get(indexOfCaesar));
+                answer.append(ch);
             }
         }
-
-        return sb.toString();
+        return answer.toString();
     }
 }
