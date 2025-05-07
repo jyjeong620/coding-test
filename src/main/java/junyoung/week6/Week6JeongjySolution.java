133599905fcea0de7b3ce25085405e1120e532a3
정준영 <jeongjy@mrblue.com>
2025-05-07 09:25:55 +0900

    refactor: 명예의 전당

--- src/main/java/junyoung/week6/Week6JeongjySolution.java
+++ src/main/java/junyoung/week6/Week6JeongjySolution.java
@@ -2,6 +2,7 @@ package junyoung.week6;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class Week6JeongjySolution {
 
@@ -21,6 +22,28 @@ public class Week6JeongjySolution {
             }
             result[i] = hallOfFame.stream().min(Integer::compareTo).get();
         }
+        return result;
+    }
+
+    public int[] refactorHallOfFame(int k, int[] score) {
+        int[] result = new int[score.length];
+        PriorityQueue<Integer> hallOfFame = new PriorityQueue<>();
+
+        for(int i = 0; i < score.length; i++) {
+            int currentScore = score[i];
+
+            if(hallOfFame.size() < k) {
+                hallOfFame.offer(currentScore);
+            }
+
+            else if(!hallOfFame.isEmpty() && hallOfFame.peek() < currentScore) {
+                hallOfFame.poll();
+                hallOfFame.offer(currentScore);
+            }
+
+            result[i] = hallOfFame.isEmpty() ? 0 : hallOfFame.peek();
+        }
+
         return result;
     }
 }
