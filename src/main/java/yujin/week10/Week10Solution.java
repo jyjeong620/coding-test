b8a86f5eb62bcbf21aeb51cf95fad5964e8afd62
devyouj <devyouj@gmail.com>
2025-06-02 15:07:25 +0900

    feat: 둘만의 암호

---
+++ src/main/java/yujin/week10/Week10Solution.java
@@ -0,0 +1,27 @@
+package yujin.week10;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Week10Solution {
+    public String secretCode(String s, String skip, int index) {
+        Set<Character> set = new HashSet<>();
+        for(char c : skip.toCharArray()) {
+            set.add(c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char value : s.toCharArray()) {
+            char result = value;
+            int count = 0;
+            while(count < index) {
+                result = (char)((result - 'a' + 1) % 26 + 'a');
+                if (!set.contains(result)) {
+                    count++;
+                }
+            }
+            sb.append(result);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/yujin/week10/Week10SolutionTest.java
@@ -0,0 +1,26 @@
+package yujin.week10;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Week10SolutionTest {
+    @DisplayName("둘만의 암호")
+    @ParameterizedTest
+    @CsvSource({
+            "aukks,wbqd,5,happy",
+            "z,abcdefghij,20,n"
+    })
+    void secretCode(String s, String skip, int index, String expected) {
+        // given
+        Week10Solution solution = new Week10Solution();
+
+        // when
+        String result = solution.secretCode(s, skip, index);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+}
