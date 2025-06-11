2f307d2044349f68ed691cde8d1bc4e4baf6b2a1
정준영 <jeongjy@mrblue.com>
2025-06-12 08:38:01 +0900

    feat: 내

--- src/main/java/junyoung/week11/Week11JeongjySolution.java
+++ src/main/java/junyoung/week11/Week11JeongjySolution.java
@@ -2,11 +2,24 @@ package junyoung.week11;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.stream.IntStream;
 
 public class Week11JeongjySolution {
     public String sortString(String s) {
         String[] splitString = s.split("");
         Arrays.sort(splitString, Comparator.reverseOrder());
         return String.join("", splitString);
+    }
+
+    public int dotProduct(int[] a, int[] b) {
+        int totalNum = 0;
+        for (int i = 0; i < a.length; i++) {
+            totalNum += a[i] * b[i];
+        }
+        return totalNum;
+    }
+
+    public int dotProduct2(int[] a, int[] b) {
+        return IntStream.range(0, a.length).map(i -> a[i] * b[i]).sum();
     }
 }
--- src/test/java/junyoung/week11/Week11JeongjySolutionTest.java
+++ src/test/java/junyoung/week11/Week11JeongjySolutionTest.java
@@ -2,7 +2,11 @@ package junyoung.week11;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,5 +26,26 @@ class Week11JeongjySolutionTest {
 
         // then
         assertThat(actual).isEqualTo(expect);
+    }
+
+    @DisplayName("내적")
+    @ParameterizedTest
+    @MethodSource("dotProductProvider")
+    void dotProductTest(int[] a, int[] b, int expect) {
+        // given
+        Week11JeongjySolution solution = new Week11JeongjySolution();
+
+        // when
+        int actual = solution.dotProduct(a, b);
+
+        // then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    private static Stream<Arguments> dotProductProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4}, new int[]{-3, -1, 0, 2}, 3),
+                Arguments.of(new int[]{-1, 0, 1}, new int[]{1, 0, -1}, -2)
+        );
     }
 }
