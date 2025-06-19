d55dd84a01c763e7c09c22b178165b096e9cc676
정준영 <jeongjy@mrblue.com>
2025-06-19 09:29:04 +0900

    feat: 다트게임

--- src/main/java/junyoung/week12/Week12JeongjySolution.java
+++ src/main/java/junyoung/week12/Week12JeongjySolution.java
@@ -1,5 +1,7 @@
 package junyoung.week12;
 
+import java.util.Arrays;
+
 public class Week12JeongjySolution {
     public int findPrimeNumber(int n) {
         int count = 0;
@@ -40,5 +42,50 @@ public class Week12JeongjySolution {
             result = result * 10 + (ch - '0');
         }
         return sign ? result : -result;
+    }
+
+    public int dartGame(String dartResult) {
+        int[] score = new int[3];
+
+        int idx = 0;
+        StringBuilder numstr = new StringBuilder();
+
+        for (int i = 0; i < dartResult.length(); i++) {
+            char c = dartResult.charAt(i);
+
+            if (c >= '0' && c <= '9') {
+                numstr.append(c);
+            } else if (c == 'S' || c == 'D' || c == 'T') {
+                idx = calculateScore(numstr, c, score, idx);
+            } else {
+                applyOption(c, score, idx);
+            }
+        }
+
+        return Arrays.stream(score).sum();
+    }
+
+    private static void applyOption(char c, int[] score, int idx) {
+        if (c == '*') {
+            score[idx - 1] *= 2;
+            if (idx - 2 >= 0) {
+                score[idx - 2] *= 2;
+            }
+        } else {
+            score[idx - 1] *= (-1);
+        }
+    }
+
+    private int calculateScore(StringBuilder numstr, char c, int[] score, int idx) {
+        int num = Integer.parseInt(numstr.toString());
+        if (c == 'S') {
+            score[idx++] = num;
+        } else if (c == 'D') {
+            score[idx++] = num * num;
+        } else {
+            score[idx++] = num * num * num;
+        }
+        numstr.setLength(0);
+        return idx;
     }
 }
--- src/test/java/junyoung/week12/Week12JeongjySolutionTest.java
+++ src/test/java/junyoung/week12/Week12JeongjySolutionTest.java
@@ -27,8 +27,8 @@ class Week12JeongjySolutionTest {
     @DisplayName("문자열을 정수로 바꾸기")
     @ParameterizedTest
     @CsvSource({
-        "+1234, 1234",
-        "-1234, -1234"
+            "+1234, 1234",
+            "-1234, -1234"
     })
     void changeToIntegerTest(String s, int expected) {
         // given
@@ -36,6 +36,28 @@ class Week12JeongjySolutionTest {
 
         // when
         int actual = solution.changeToInteger(s);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("다트게임")
+    @ParameterizedTest
+    @CsvSource({
+            "1S2D*3T, 37",
+            "1D2S#10S, 9",
+            "1D2S0T, 3",
+            "1S*2T*3S, 23",
+            "1D#2S*3S, 5",
+            "1T2D3D#, -4",
+            "1D2S3T*, 59",
+    })
+    void dartGameTest(String s, int expected) {
+        // given
+        Week12JeongjySolution solution = new Week12JeongjySolution();
+
+        // when
+        int actual = solution.dartGame(s);
 
         // then
         assertThat(actual).isEqualTo(expected);
