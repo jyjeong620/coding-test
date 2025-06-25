66e75e703d6610dab983b596feebaeb217e3bdb8
정준영 <jeongjy@mrblue.com>
2025-06-25 09:08:42 +0900

    feat: 이상한문자 만들기

--- src/main/java/junyoung/week13/Week13JeongjySolution.java
+++ src/main/java/junyoung/week13/Week13JeongjySolution.java
@@ -6,13 +6,31 @@ public class Week13JeongjySolution {
         n %= 26;
         for (char c : s.toCharArray()) {
             if (c >= 'a' && c <= 'z') {
-                sb.append((char)('a' + (c - 'a' + n) % 26));
+                sb.append((char) ('a' + (c - 'a' + n) % 26));
             } else if (c >= 'A' && c <= 'Z') {
-                sb.append((char)('A' + (c - 'A' + n) % 26));
+                sb.append((char) ('A' + (c - 'A' + n) % 26));
             } else {
                 sb.append(c);
             }
         }
         return sb.toString();
+    }
+
+    public String makeWeirdText(String s) {
+        String[] words = s.split(" ", -1);
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                result.append(" ");
+            }
+            String word = words[i];
+            for (int j = 0; j < word.length(); j++) {
+                char c = word.charAt(j);
+                result.append(j % 2 == 0 ? Character.toUpperCase(c) : Character.toLowerCase(c));
+            }
+        }
+
+        return result.toString();
     }
 }
--- src/test/java/junyoung/week13/Week13JeongjySolutionTest.java
+++ src/test/java/junyoung/week13/Week13JeongjySolutionTest.java
@@ -1,5 +1,6 @@
 package junyoung.week13;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,20 +8,37 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class Week13JeongjySolutionTest {
+    private Week13JeongjySolution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Week13JeongjySolution();
+    }
 
     @DisplayName("시저암호")
     @ParameterizedTest
-    @CsvSource ({
+    @CsvSource({
             "AB, 1, BC",
             "z, 1, a",
             "a B z, 4, e F d"
     })
     void caesarCipherTest(String s, int n, String expected) {
-        // given
-        Week13JeongjySolution solution = new Week13JeongjySolution();
-
         // when
         String actual = solution.caesarCipher(s, n);
+
+        // then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("이상한문자만들기")
+    @ParameterizedTest
+    @CsvSource({
+            "try hello world, TrY HeLlO WoRlD",
+            "' read the explanation carefully  ', ' ReAd ThE ExPlAnAtIoN CaReFuLlY  '"
+    })
+    void makeWeirdTextTest(String s, String expected) {
+        // when
+        String actual = solution.makeWeirdText(s);
 
         // then
         assertThat(actual).isEqualTo(expected);
