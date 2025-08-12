b485a24e29dd5d0a75ba834de5f33158d68300f0
devyouj <devyouj@gmail.com>
2025-08-12 16:50:20 +0900

    feat: 행렬의 덧셈

---
+++ src/test/java/yujin/week20/Week20SolutionTest.java
@@ -0,0 +1,46 @@
+package yujin.week20;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Week20SolutionTest {
+    private Week20Solution solution;
+
+    @BeforeEach
+    void setUp() {
+        solution = new Week20Solution();
+    }
+
+    @DisplayName("행렬의 덧셈")
+    @ParameterizedTest
+    @MethodSource("arraySumTestCases")
+    void arraySum(int[][] arr1, int[][] arr2, int[][] expected) {
+        // when
+        int[][] result = solution.arraySum(arr1, arr2);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> arraySumTestCases() {
+        return Stream.of(
+                Arguments.of(
+                        new int[][]{{1, 2}, {2, 3}},
+                        new int[][]{{3, 4}, {5, 6}},
+                        new int[][]{{4, 6}, {7, 9}}
+                ),
+                Arguments.of(
+                        new int[][]{{1}, {2}},
+                        new int[][]{{3}, {4}},
+                        new int[][]{{4}, {6}}
+                )
+        );
+    }
+}
